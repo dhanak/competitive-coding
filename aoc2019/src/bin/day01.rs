5d3f4aef79b2d18579b98b8b95b22e5280933424
David Hanak <dhanak@gmail.com>
2022-01-09 21:57:50 +0100

    [AoC2019] Day 1
    This time while learning Rust!

---
+++ aoc2019/src/bin/day01.rs
@@ -0,0 +1,20 @@
+extern crate aoc2019;
+
+use std::iter::successors;
+
+use aoc2019::utils::read_lines;
+
+fn main() {
+    let fuel: Vec<i64> = read_lines("day01.in")
+        .map(|line| line.unwrap().parse::<i64>().unwrap())
+        .map(|val| val / 3 - 2)
+        .collect();
+    println!("Q1: {}", fuel.iter().sum::<i64>());
+
+    let fuel = fuel.iter().map(|&v| {
+        successors(Some(v), |&v| Some(v / 3 - 2))
+            .take_while(|&v| v > 0)
+            .sum::<i64>()
+    });
+    println!("Q2: {}", fuel.sum::<i64>());
+}
---
+++ aoc2019/src/lib.rs
@@ -0,0 +1,14 @@
+pub mod utils {
+    use std::fs::File;
+    use std::io::{BufRead, BufReader, Lines};
+    use std::path::Path;
+
+    // Returns an Iterator to the Reader of the lines of the file.
+    pub fn read_lines<P>(filename: P) -> Lines<BufReader<File>>
+    where
+        P: AsRef<Path>,
+    {
+        let file = File::open(filename).unwrap();
+        BufReader::new(file).lines()
+    }
+}
