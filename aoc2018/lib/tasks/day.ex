7c52c946b25dc38072d1e46457195add70a91c1e
David Hanak <dhanak@gmail.com>
2024-10-25 18:59:11 +0200

    [AoC2018] Template and Day01 in Elixir

---
+++ aoc2018/lib/tasks/day.ex
@@ -0,0 +1,44 @@
+defmodule Mix.Tasks.Day do
+  @shortdoc "AoC 2018 Day"
+
+  @test ""
+
+  import ExUnit.Assertions
+  use Mix.Task
+
+  def q1(_) do
+    0
+  end
+
+  def q2(_) do
+    0
+  end
+
+  defp check() do
+    assert q1(@test) == 0
+    assert q2(@test) == 0
+  end
+
+  def run(_) do
+    check()
+
+    day =
+      __MODULE__
+      |> Atom.to_string()
+      |> String.split(".")
+      |> List.last()
+      |> String.downcase()
+
+    lines =
+      File.stream!("input/#{day}.txt", :line)
+      |> Stream.map(&String.trim_trailing(&1, "\n"))
+
+    {time, _} =
+      :timer.tc(fn ->
+        IO.puts("Q1: #{q1(lines)}")
+        IO.puts("Q2: #{q2(lines)}")
+      end)
+
+    IO.puts("  #{time / 1_000_000} seconds")
+  end
+end
---
+++ aoc2018/lib/tasks/day01.ex
@@ -0,0 +1,59 @@
+defmodule Mix.Tasks.Day01 do
+  @shortdoc "AoC 2018 Day 1"
+
+  @test "+1, -2, +3, +1"
+
+  import ExUnit.Assertions
+  use Mix.Task
+
+  def q1(values) do
+    Enum.sum(values)
+  end
+
+  def q2(values) do
+    values
+    |> Stream.cycle()
+    |> Stream.transform({MapSet.new(), 0}, fn v, {set, sum} ->
+      if MapSet.member?(set, sum) do
+        {:halt, {set, sum}}
+      else
+        {[sum + v], {MapSet.put(set, sum), sum + v}}
+      end
+    end)
+    |> Enum.take(-1)
+    |> List.first()
+  end
+
+  defp check() do
+    values =
+      @test
+      |> String.split(", ")
+      |> Stream.map(&elem(Integer.parse(&1), 0))
+
+    assert q1(values) == 3
+    assert q2(values) == 2
+  end
+
+  def run(_) do
+    check()
+
+    day =
+      __MODULE__
+      |> Atom.to_string()
+      |> String.split(".")
+      |> List.last()
+      |> String.downcase()
+
+    lines =
+      File.stream!("input/#{day}.txt", :line)
+      |> Stream.map(&elem(Integer.parse(&1), 0))
+
+    {time, _} =
+      :timer.tc(fn ->
+        IO.puts("Q1: #{q1(lines)}")
+        IO.puts("Q2: #{q2(lines)}")
+      end)
+
+    IO.puts("  #{time / 1_000_000} seconds")
+  end
+end
