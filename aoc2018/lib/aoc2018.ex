56cc59bee3defb6efcdd1cc3d79a62db99815a20
David Hanak <dhanak@gmail.com>
2024-11-03 18:05:38 +0100

    [AoC2018] Day 06, Part 1

--- aoc2018/lib/aoc2018.ex
+++ aoc2018/lib/aoc2018.ex
@@ -20,4 +20,43 @@ defmodule Aoc2018 do
       "A"
   """
   def upcase(c), do: c &&& bnot(32)
+
+  @doc """
+  Compute the Manhattan distance between two coordinates
+
+  ## Examples
+
+    iex> manhattan_distance({0, 0}, {2, 3})
+    5
+    iex> manhattan_distance({1, 4}, {-2, 7})
+    6
+  """
+  def manhattan_distance({ax, ay}, {bx, by}), do: abs(ax - bx) + abs(ay - by)
+
+  @doc """
+  Return the value and the index of the smallest element in an enumerable.
+
+  ## Examples
+
+    iex> argmin([1, 2, 3])
+    {1, 0}
+
+    iex> argmin([2, 1, 3])
+    {1, 1}
+
+    iex> argmin([~D[2017-03-31], ~D[2017-04-01]])
+    {~D[2017-04-01], 1}
+
+    iex> argmin([~D[2017-03-31], ~D[2017-04-01]], Date)
+    {~D[2017-03-31], 0}
+  """
+  def argmin(
+        enum,
+        sorter \\ &<=/2,
+        empty_fallback \\ fn -> raise Enum.EmptyError end
+      ) do
+    enum
+    |> Enum.with_index()
+    |> Enum.min_by(&elem(&1, 0), sorter, empty_fallback)
+  end
 end
---
+++ aoc2018/lib/tasks/day06.ex
@@ -0,0 +1,109 @@
+defmodule Mix.Tasks.Day06 do
+  @shortdoc "AoC 2018 Day 6"
+
+  @test """
+        1, 1
+        1, 6
+        8, 3
+        3, 4
+        5, 5
+        8, 9
+        """
+        |> String.split("\n", trim: true)
+
+  import Aoc2018
+  import ExUnit.Assertions
+  use Mix.Task
+
+  def parse_coordinate(line) do
+    [x, y] =
+      line
+      |> String.split(", ")
+      |> Enum.map(fn s ->
+        {i, ""} = Integer.parse(s)
+        i
+      end)
+
+    {x, y}
+  end
+
+  def q1(coords) do
+    {l, r} = coords |> Enum.map(&elem(&1, 0)) |> Enum.min_max()
+    {t, b} = coords |> Enum.map(&elem(&1, 1)) |> Enum.min_max()
+
+    grid =
+      for x <- l..r, y <- t..b do
+        {cs, _} =
+          Enum.reduce(coords, {[], Inf}, fn coord, {min_cs, min_d} ->
+            d = manhattan_distance({x, y}, coord)
+
+            cond do
+              d < min_d -> {[coord], d}
+              d == min_d -> {[coord | min_cs], d}
+              d > min_d -> {min_cs, min_d}
+            end
+          end)
+
+        {{x, y}, cs}
+      end
+      |> Map.new()
+
+    edge =
+      (for(x <- l..r, do: [{x, t}, {x, b}]) ++
+         for(y <- t..b, do: [{l, y}, {r, y}]))
+      |> Enum.concat()
+      |> Enum.flat_map(fn c ->
+        case grid[c] do
+          [v] -> [v]
+          _ -> []
+        end
+      end)
+      |> MapSet.new()
+
+    {_, s} =
+      grid
+      |> Map.values()
+      |> Enum.flat_map(fn
+        [c] -> if MapSet.member?(edge, c), do: [], else: [c]
+        _ -> []
+      end)
+      |> Enum.frequencies()
+      |> Enum.max_by(&elem(&1, 1))
+
+    s
+  end
+
+  def q2(_) do
+    0
+  end
+
+  defp check() do
+    coords = Enum.map(@test, &parse_coordinate/1)
+    assert q1(coords) == 17
+    assert q2(coords) == 0
+  end
+
+  def run(_) do
+    check()
+
+    day =
+      __MODULE__
+      |> Atom.to_string()
+      |> String.split(".")
+      |> List.last()
+      |> String.downcase()
+
+    lines =
+      File.stream!("input/#{day}.txt", :line)
+      |> Stream.map(&String.trim_trailing(&1, "\n"))
+      |> Stream.map(&parse_coordinate/1)
+
+    {time, _} =
+      :timer.tc(fn ->
+        IO.puts("Q1: #{q1(lines)}")
+        IO.puts("Q2: #{q2(lines)}")
+      end)
+
+    IO.puts("  #{time / 1_000_000} seconds")
+  end
+end
