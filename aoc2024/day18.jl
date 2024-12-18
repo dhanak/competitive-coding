28b46eed618b13dae2a986fb98f4dcf60d13821c
David Hanak <dhanak@gmail.com>
2024-12-18 17:57:45 +0100

    [AoC2024] Day 18 - add fast(er) alternative solution

--- aoc2024/day18.jl
+++ aoc2024/day18.jl
@@ -1,6 +1,7 @@
 using Test: @testset, @test
 
 using aoc2024
+using DataStructures: PriorityQueue, dequeue!
 
 test = """
        5,4
@@ -55,12 +56,48 @@ function q1(bytes, s)
     return nothing
 end
 
-function q2(bytes, n, s)
+# quick and easy solution that tries to solve the maze once for each block
+function q2_short(bytes, n, s)
     i = findfirst(n + 1:length(bytes)) do j
         return q1(view(bytes, 1:j), s) === nothing
     end
     return join(bytes[n + i].I .- 1, ',')
 end
+
+# fast solution with twoway search and priority queue based block removal
+function q2_fast(bytes, _, s)
+    M = fill(0, s)
+    M[bytes] .= eachindex(bytes)
+    M = grow(M, -1)
+    blocks = PriorityQueue{CI, Int}()
+    flood!(M, blocks, -2, CI(2, 2))
+    flood!(M, blocks, -3, CI(s .+ 1))
+    while !isempty(blocks)
+        p = dequeue!(blocks)
+        ns = filter(<(-1), M[p .+ neighbors4]) |> unique! |> sort!
+        ns == [-3, -2] && return join(p.I .- 2, ',')
+        M[p] = 0
+        flood!(M, blocks, only(ns), p)
+    end
+end
+
+function flood!(M, blocks, d, p)
+    queue = [p]
+    while !isempty(queue)
+        p = popfirst!(queue)
+        M[p] != 0 && continue
+        M[p] = d
+        for n in p .+ neighbors4
+            if M[n] == 0
+                push!(queue, n)
+            elseif M[n] > 0
+                blocks[n] = -M[n]
+            end
+        end
+    end
+end
+
+const q2 = q2_fast
 
 if !isinteractive()
     @testset begin
